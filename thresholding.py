4bf4f498221afb2d76b35dbc83c2d9c30fe5988d
sammitjain <sammit.bitspilani@gmail.com>
2017-07-26 13:15:30 +0530

    Showing all results

--- thresholding.py
+++ thresholding.py
@@ -5,6 +5,7 @@ import numpy as np
 
 testImg = cv2.imread('bookpage.jpg')
 
+
 #Thresholding normally
 retval, threshold = cv2.threshold(testImg, 12, 255,cv2.THRESH_BINARY)
 
@@ -13,6 +14,7 @@ grayImg = cv2.cvtColor(testImg,cv2.COLOR_BGR2GRAY)
 
 #Using a Gaussian Adaptive Threshold
 gthreshold = cv2.adaptiveThreshold(grayImg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+
 
 cv2.imshow('Original',testImg)
 cv2.imshow('Threshold',threshold)
